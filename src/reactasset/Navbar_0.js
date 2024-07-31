7c82469579bbc4bc369649abc016a22e84ac6213
halfeastx7 <hafizizan@gmail.com>
2024-07-31 09:02:34 +0800

    rename

--- src/reactasset/Navbar_0.js
+++ src/reactasset/Navbar_0.js
@@ -43,7 +43,7 @@ function Navbar_0() {
     return (
         <div>
               <nav class="navbar navbar-expand-lg navbar-warning bg-warning ">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">NexusForge</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
